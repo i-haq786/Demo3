bbbae2b7ac0701a2f096be103d21251a874bbbb2
i-haq786 <i.haqcs@gmail.com>
2023-12-21 06:18:10 +0530

    Initial Commit

---
+++ Demo3/Demo3App.swift
@@ -0,0 +1,17 @@
+//
+//  Demo3App.swift
+//  Demo3
+//
+//  Created by Inzamam on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Demo3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
